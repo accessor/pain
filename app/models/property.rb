fe193f3f851069d761ec4b2aea7baa1beeb86d6e
Rahul <trahul023@gmail.com>
2013-09-28 09:09:23 +0530

    Adding fileds and styling

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,15 +1,19 @@
 class Property < ActiveRecord::Base
-  attr_accessible  :title, :description, :dimensions, :price, :status, :expiry_date, :address_attributes, :image_attributes
+  attr_accessible  :title, :description, :dimensions, :price, :status, :expiry_date, :address_attributes, :image_attributes, :amenity_list
   belongs_to :user
   belongs_to :address, dependent: :destroy
   belongs_to :image, dependent: :destroy
   accepts_nested_attributes_for :address
   accepts_nested_attributes_for :image
-  before_create :assign_user
+  before_create :assign_user, :generate_code
   acts_as_taggable_on :amenities
 
   private
     def assign_user
       self.user = current_user
+    end
+
+    def generate_code
+      self.code = "PA#{Time.new.to_i + Property.count}"
     end
 end
