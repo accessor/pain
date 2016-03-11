c0c3c40d5678a35b2d3ed32c6a7dec3b580dc257
Rahul <trahul023@gmail.com>
2016-03-11 08:16:06 +0530

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "trahul023@gmail.com",
-  :password             => "chamcham200",
+  :password             => "",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
